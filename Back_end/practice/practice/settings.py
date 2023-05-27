60b547a5a59a27c1e718b170c3c4951d494e2a97
T0kas75 <114758175+T0kas75@users.noreply.github.com>
2023-05-27 22:28:59 +0300

    prod version

--- Back_end/practice/practice/settings.py
+++ Back_end/practice/practice/settings.py
@@ -27,9 +27,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-=0he*a8ez5j678+9txcix2a!y6270l16smh-emgk6^b9+$g12+'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
